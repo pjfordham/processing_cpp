a638e39edab9760773af3c57937a3025c3fd267d
Peter Fordham <peter.fordham@gmail.com>
2023-02-24 10:22:23 -0800

    Remove processing.h from each sketch.

--- sketch.cc
+++ sketch.cc
@@ -1,5 +1,3 @@
-#include "processing.h"
-
 /**
  * Constrain.
  *
--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 file="${1:-sketch.cc}"
-g++ $file -lSDL2 -lSDL2_gfx -lm && ./a.out
+g++ -include processing.h $file -lSDL2 -lSDL2_gfx -lm && ./a.out
