dca2589e6ca9f808ef9d48224863b0750bb4a57f
Peter Fordham <peter.fordham@gmail.com>
2023-06-12 15:16:27 -0700

    Fix TextRendering demo.

--- examples/Demos/Performance/TextRendering/TextRendering.cc
+++ examples/Demos/Performance/TextRendering/TextRendering.cc
@@ -1,15 +1,15 @@
 
-public void setup() {
+void setup() {
   size(800, 600, P2D);
   fill(0);
 }
 
-public void draw() {
+void draw() {
   background(255);
   for (int i = 0; i < 10000; i++) {
     float x = random(width);
     float y = random(height);
     text("HELLO", x, y);
   }
-  if (frameCount % 10 == 0) println(frameRate);
+  // if (frameCount % 10 == 0) println(frameRate);
 }
