99e6de1f9c0b2d514ade082ac4c38e52b626b020
Peter Fordham <peter.fordham@gmail.com>
2023-06-18 17:59:05 -0700

    Remove unused files.

--- debug.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -f *~ weak.o sketch.o sketch
-file="${1:-sketch.cc}"
-g++ -c -g PerlinNoise.cc -o PerlinNoise.o
-g++ -c -g weak.cc -o weak.o
-g++ -c -I/usr/include/freetype2 -g -include processing.h $file  -o sketch.o
-g++ sketch.o weak.o PerlinNoise.o -lSDL2 -lSDL2_ttf -lSDL2_image -lfreetype -lglfw -lGLEW -lGL -lSDL2main -lcurl -lfmt -lm -o ./sketch && gdb ./sketch
--- valgrind.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -f *~ weak.o sketch.o sketch
-file="${1:-sketch.cc}"
-g++ -std=c++17 -c -g PerlinNoise.cc -o PerlinNoise.o
-g++ -std=c++17 -c -g weak.cc -o weak.o
-g++ -std=c++17 -c -I/usr/include/freetype2 -g -include processing.h $file  -o sketch.o
-g++ -std=c++17 sketch.o weak.o PerlinNoise.o -lSDL2 -lSDL2_ttf -lSDL2_image -lfreetype -lglfw -lGLEW -lGL -lSDL2main -lcurl -lfmt -lm -o ./sketch && valgrind --num-callers=30 ./sketch
