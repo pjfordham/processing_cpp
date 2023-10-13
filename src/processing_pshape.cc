64d37acd1fcf7677a7532c6f76c876891f93ada2
Peter Fordham <peter.fordham@gmail.com>
2023-10-12 23:04:13 -0700

    Use libtess2 for polygon triangulation.

--- src/processing_pshape.cc
+++ src/processing_pshape.cc
@@ -1,117 +1,86 @@
 #include "processing_pshape.h"
 #include "processing_math.h"
 #include <vector>
+#include <tesselator.h>
 
-struct indexed_PVector : public PVector {
-   unsigned short int i;
-   indexed_PVector(PVector v,unsigned short i_) : PVector(v), i(i_) {}
-};
-
-bool isPointInTriangle(const indexed_PVector &point, const indexed_PVector &v0,
-                       const indexed_PVector &v1, const indexed_PVector &v2) {
-   // Calculate barycentric coordinates of the point with respect to the triangle
-   float alpha = ((v1.y - v2.y) * (point.x - v2.x) + (v2.x - v1.x) * (point.y - v2.y)) /
-      ((v1.y - v2.y) * (v0.x - v2.x) + (v2.x - v1.x) * (v0.y - v2.y));
-   float beta = ((v2.y - v0.y) * (point.x - v2.x) + (v0.x - v2.x) * (point.y - v2.y)) /
-      ((v1.y - v2.y) * (v0.x - v2.x) + (v2.x - v1.x) * (v0.y - v2.y));
-   float gamma = 1.0 - alpha - beta;
-   // Check if the barycentric coordinates are all positive
-   return alpha >= 0 && beta >= 0 && gamma >= 0;
-}
-
-bool isEar(const std::vector<indexed_PVector>& polygon, int i) {
-   int n = polygon.size();
-   int pi = (i + n - 1) % n;
-   int ni = (i + 1) % n;
-   // Check if there are any other vertices inside the triangle
-   for (int j = 0; j < n; j++) {
-      if (j != i && j != ni && j != pi) {
-         if (isPointInTriangle(polygon[j], polygon[pi], polygon[i], polygon[ni])) {
-            return false;
-         }
-      }
-   }
-   return true;
-}
-
-bool isConvexPVector(const std::vector<indexed_PVector>& polygon, int i) {
-   int n = polygon.size();
-   PVector prev = polygon[(i + n - 1) % n];
-   PVector curr = polygon[i];
-   PVector next = polygon[(i + 1) % n];
-   double crossProduct = (curr.x - prev.x) * (next.y - curr.y) - (curr.y - prev.y) * (next.x - curr.x);
-   return !(crossProduct < 0);
-}
-
-int findEar(const std::vector<indexed_PVector>& polygon) {
-   int n = polygon.size();
-   for (int i = 0; i < n; i++) {
-      // Check if vertex i is a convex vertex
-      if (isConvexPVector(polygon, i)) {
-         // Check if the triangle i-1, i, i+1 is an ear
-         if (isEar(polygon, i)) {
-            return i;
-         }
-      }
-   }
-   // If no ear is found, return the first vertex
-   return 0;
-}
-
-bool isClockwise(const std::vector<gl_context::vertex> &polygon) {
-   if (polygon.size() < 3)
+bool PShape::isClockwise() const {
+   if ( style != POLYGON && style != QUADS )
+      return false;
+   if (vertices.size() < 3)
       return true;
 
-   auto current = polygon[0];
-   auto prev = polygon[polygon.size()-1];
+   auto current = vertices[0];
+   auto prev = vertices[vertices.size()-1];
    int sum = (current.position.x - prev.position.x) * (current.position.y + prev.position.y);
-   for( int i = 1 ; i < polygon.size() ; ++i) {
-      auto current = polygon[i];
-      auto prev = polygon[i-1];
+   for( int i = 1 ; i < vertices.size() ; ++i) {
+      auto current = vertices[i];
+      auto prev = vertices[i-1];
       sum += (current.position.x - prev.position.x) * (current.position.y + prev.position.y);
    }
    return sum < 0;
 }
 
-bool PShape::isClockwise() const {
-   if ( style != POLYGON && style != QUADS )
-      return false;
-   return ::isClockwise( vertices );
-}
+static std::vector<unsigned short> triangulatePolygon(const std::vector<gl_context::vertex> &vertices) {
 
-std::vector<unsigned short> triangulatePolygon(const std::vector<gl_context::vertex> &polygon_) {
-   std::vector<indexed_PVector> polygon;
-   if (isClockwise(polygon_)) {
-      int index = 0;
-      for (auto &&vertex : polygon_) {
-         polygon.emplace_back( vertex.position, index++ );
-      }
-   } else {
-      int index = polygon_.size() - 1;
-      for (auto v = polygon_.rbegin(); v != polygon_.rend(); v++ ) {
-         polygon.emplace_back( v->position, index-- );
-      }
+   if (vertices.size() < 3) {
+      return {}; // empty vector
    }
+
+   std::vector<float> pointlist;
+   std::vector<int> markerlist;
+   for ( auto &v : vertices ) {
+      pointlist.push_back(v.position.x);
+      pointlist.push_back(v.position.y);
+      pointlist.push_back(v.position.z);
+   }
+
+   TESSalloc ma;
+   int allocated = 0;
+   memset(&ma, 0, sizeof(ma));
+   ma.memalloc = [] (void* userData, unsigned int size) {
+      int* allocated = ( int*)userData;
+      TESS_NOTUSED(userData);
+      *allocated += (int)size;
+      return malloc(size);
+   };
+   ma.memfree = [] (void* userData, void* ptr) {
+      TESS_NOTUSED(userData);
+      free(ptr);
+   };
+   ma.userData = (void*)&allocated;
+   ma.extraVertices = 256; // realloc not provided, allow 256 extra vertices.
+
+   TESStesselator* tess = tessNewTess(&ma);
+   if (!tess)
+      abort();
+
+   tessSetOption(tess, TESS_CONSTRAINED_DELAUNAY_TRIANGULATION, 1);
+   const int nvp = 3;
+
+   tessAddContour(tess, 3, pointlist.data(), sizeof(float)*3, pointlist.size()/3, 0);
+
+   if (!tessTesselate(tess, TESS_WINDING_POSITIVE, TESS_POLYGONS, nvp, 3, 0))
+      abort();
+
+   // Draw tesselated pieces.
+   const float* verts = tessGetVertices(tess);
+   const int* vinds = tessGetVertexIndices(tess);
+   const int* elems = tessGetElements(tess);
+   const int nverts = tessGetVertexCount(tess);
+   const int nelems = tessGetElementCount(tess);
 
    std::vector<unsigned short> triangles;
-   if (polygon.size() < 3) {
-      return triangles; // empty vector
+
+   for (int i = 0; i < nelems; ++i)
+   {
+      const int* p = &elems[i*nvp];
+      for (int j = 0; j < nvp && p[j] != TESS_UNDEF; ++j) {
+         triangles.push_back( vinds[ p[j] ] );
+      }
    }
-   while (polygon.size() > 3) {
-      int i = findEar(polygon); // find an ear of the polygon
-      // add the ear as separate triangles to the output vector
-      int n = polygon.size();
-      int pi = (i + n - 1) % n;
-      int ni = (i + 1) % n;
-      triangles.push_back(polygon[pi].i);
-      triangles.push_back(polygon[i].i);
-      triangles.push_back(polygon[ni].i);
-      polygon.erase(polygon.begin() + i); // remove the ear vertex from the polygon
-   }
-   // add the final triangle to the output vector
-   triangles.push_back(polygon[0].i);
-   triangles.push_back(polygon[1].i);
-   triangles.push_back(polygon[2].i);
+
+   tessDeleteTess(tess);
+
    return triangles;
 }
 
