8dc6c5691ba6c8c4c6b3daad2b23f3e5e1802b5a
Peter Fordham <peter.fordham@gmail.com>
2023-11-05 21:20:18 -0800

    Update LightFalloff example.

--- examples/Pete/LightFalloff.cc
+++ examples/Pete/LightFalloff.cc
@@ -1,19 +1,11 @@
-void setup() {
-   size(400, 400, P3D);
-}
-
-void draw() {
-   //lights();
-   noStroke();
-   background(0);
-   ambientLight(0,0,0);
-   directionalLight(0, 0, 0, 200, 200, 200);
-   lightFalloff(1.0, 0.5, 0.0);
-   pointLight(15, 25, 15, mouseX, 200, 200);
-   beginShape();
-   vertex(0, 0, 0);
-   vertex(400, 0, -400);
-   vertex(400, 400, -400);
-   vertex(0, 400, 0);
-   endShape(CLOSE);
-}
+size(400, 400, P3D);
+noStroke();
+background(0);
+lightFalloff(1.0, 0.001, 0.0);
+pointLight(150, 250, 150, 200, 200, 200);
+beginShape();
+vertex(0, 0, 0);
+vertex(400, 0, -400);
+vertex(400, 400, -400);
+vertex(0, 400, 0);
+endShape(CLOSE);
