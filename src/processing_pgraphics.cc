0dc63a80b329629aed2c8049c4cb666526fd52de
Peter Fordham <peter.fordham@gmail.com>
2025-02-03 23:10:02 -0800

    Fix triangle windings for rectangles and cubes.

--- src/processing_pgraphics.cc
+++ src/processing_pgraphics.cc
@@ -463,9 +463,9 @@ public:
       cube.vertex( -w,  h, -d, 0.0, 1.0 );
 
       cube.populateIndices( {
-            0,1,2, 0,2,3, 4,5,6, 4,6,7,
-            8,9,10, 8,10,11, 12,13,14, 12,14,15,
-            16,17,18, 16,18,19, 20,21,22, 20,22,23
+            2,1,0, 3,2,0, 6,5,4, 7,6,4,
+            10,9,8, 11,10,8, 14,13,12, 15,14,12,
+            18,17,16, 19,18,16, 22,21,20, 23,22,20
          } );
 
       cube.endShape();
@@ -757,7 +757,7 @@ public:
       shape.vertex(x+width,y);
       shape.vertex(x+width,y+height);
       shape.vertex(x,y+height);
-      shape.populateIndices( { 0,1,2,0,2,3 } );
+      shape.populateIndices( { 2,1,0,3,2,0 } );
       shape.endShape(CLOSE);
       return shape;
    }
