6ac47f491bf0d8ad8f5d43264e111452156a94c2
Peter Fordham <peter.fordham@gmail.com>
2025-03-11 21:58:31 -0700

    Use a ThreadPool with a single thread as a render thread and launch all GL calls from that thread via lambda's. Sync where necessary but allow CPU to start work on next frame while GPU is drawing previous one.

--- src/processing_opengl_framebuffer.cc
+++ src/processing_opengl_framebuffer.cc
@@ -4,12 +4,15 @@
 #include <sstream>     // For std::stringstream
 
 #include <fmt/core.h>
+#include <GLFW/glfw3.h>
+#include <thread>
 
 #include "processing_enum.h"
 #include "processing_opengl.h"
 #include "processing_opengl_framebuffer.h"
 #include "processing_debug.h"
 #include "processing_opengl_shader.h"
+#include "processing_task_queue.h"
 
 #undef DEBUG_METHOD
 #define DEBUG_METHOD() do {} while (false)
@@ -96,6 +99,8 @@ namespace gl {
          abort();
       }
 
+      renderThread.enqueue( [&] {
+
       glGenFramebuffers(1, &id);
       bind();
 
@@ -111,8 +116,8 @@ namespace gl {
       } else {
          glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, width, height);
       }
-
       glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthBufferID);
+      glBindRenderbuffer(GL_RENDERBUFFER, 0);
 
       glActiveTexture(GL_TEXTURE0);
 
@@ -122,6 +127,7 @@ namespace gl {
          glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, colorBufferID);
          glTexImage2DMultisample(GL_TEXTURE_2D_MULTISAMPLE, aaFactor, GL_RGBA, width, height, GL_TRUE);
          glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D_MULTISAMPLE, colorBufferID, 0);
+         glBindTexture(GL_TEXTURE_2D_MULTISAMPLE, 0);
       } else {
          glBindTexture(GL_TEXTURE_2D, colorBufferID);
          glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
@@ -130,6 +136,7 @@ namespace gl {
          glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
          glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
          glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colorBufferID, 0);
+         glBindTexture(GL_TEXTURE_2D, 0);
       }
 
       auto err = glCheckFramebufferStatus(GL_FRAMEBUFFER);
@@ -152,6 +159,7 @@ namespace gl {
          glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
          glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, textureBufferID, 0);
+         glBindTexture(GL_TEXTURE_2D, 0);
 
          auto err = glCheckFramebufferStatus(GL_FRAMEBUFFER);
          if (err != GL_FRAMEBUFFER_COMPLETE) {
@@ -161,16 +169,20 @@ namespace gl {
          glClearColor(0, 0, 0, 1);
          glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
       }
+      } );
+      renderThread.wait_until_nothing_in_flight();
    }
 
    texture_ptr framebuffer::getColorBufferID() {
       if (aaMode == MSAA) {
-         glBindFramebuffer(GL_READ_FRAMEBUFFER, id);
-         glBindFramebuffer(GL_DRAW_FRAMEBUFFER, did);
-         glBlitFramebuffer(0, 0, width, height, 0, 0, width, height,
-                           GL_COLOR_BUFFER_BIT, GL_LINEAR);
-         glBindFramebuffer(GL_READ_FRAMEBUFFER, 0);
-         glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+         renderThread.enqueue([width=width, height=height, id=id, did=did] {
+            glBindFramebuffer(GL_READ_FRAMEBUFFER, id);
+            glBindFramebuffer(GL_DRAW_FRAMEBUFFER, did);
+            glBlitFramebuffer(0, 0, width, height, 0, 0, width, height,
+                              GL_COLOR_BUFFER_BIT, GL_LINEAR);
+            glBindFramebuffer(GL_READ_FRAMEBUFFER, 0);
+            glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+         } );
          return std::make_shared<texture_t>(textureBufferID);
       } else {
          return std::make_shared<texture_t>(colorBufferID);
@@ -179,51 +191,86 @@ namespace gl {
 
    framebuffer::~framebuffer() {
       DEBUG_METHOD();
-      if (textureBufferID)
-         glDeleteTextures(1, &textureBufferID);
-      if (depthBufferID)
-         glDeleteRenderbuffers(1, &depthBufferID);
-      if (colorBufferID)
-         glDeleteTextures(1, &colorBufferID);
-      if (id)
-         glDeleteFramebuffers(1, &id);
-      if (did)
-         glDeleteFramebuffers(1, &did);
+
+      if (id) {
+         renderThread.enqueue([&] {
+            if (id) {
+               glBindFramebuffer(GL_FRAMEBUFFER, id);
+               glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, 0);
+               if (aaMode == MSAA) {
+                  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D_MULTISAMPLE, 0, 0);
+               } else {
+                  glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, 0, 0);
+               }
+               glBindFramebuffer(GL_FRAMEBUFFER, 0);
+               glDeleteFramebuffers(1, &id);
+            }
+
+            if (did) {
+               glBindFramebuffer(GL_FRAMEBUFFER, did);
+               glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, 0, 0);
+               glBindFramebuffer(GL_FRAMEBUFFER, 0);
+               glDeleteFramebuffers(1, &did);
+            }
+
+            if (textureBufferID)
+               glDeleteTextures(1, &textureBufferID);
+            if (depthBufferID)
+               glDeleteRenderbuffers(1, &depthBufferID);
+            if (colorBufferID)
+               glDeleteTextures(1, &colorBufferID);
+
+            glBindFramebuffer(GL_FRAMEBUFFER, 0);
+         });
+         renderThread.wait_until_nothing_in_flight();
+      }
    }
 
    void framebuffer::blit(framebuffer &dest) const {
       DEBUG_METHOD();
       if (id != dest.id) {
-         glBindFramebuffer(GL_READ_FRAMEBUFFER, id);
-         glBindFramebuffer(GL_DRAW_FRAMEBUFFER, dest.id);
-         glBlitFramebuffer(0,0,width,height,0,0,dest.width,dest.height,GL_COLOR_BUFFER_BIT,GL_LINEAR);
-         glBindFramebuffer(GL_READ_FRAMEBUFFER, 0);
-         glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
-      }
+         renderThread.enqueue([width=width,height=height,id=id,dwidth=dest.width, dheight=dest.height, did=dest.id] {
+          glBindFramebuffer(GL_READ_FRAMEBUFFER, id);
+          glBindFramebuffer(GL_DRAW_FRAMEBUFFER, did);
+          glBlitFramebuffer(0, 0, width, height, 0, 0, dwidth, dheight,
+                            GL_COLOR_BUFFER_BIT, GL_LINEAR);
+          glBindFramebuffer(GL_READ_FRAMEBUFFER, 0);
+          glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+        });
+        }
    }
 
    void framebuffer::updatePixels( const std::vector<unsigned int> &pixels ) {
       DEBUG_METHOD();
-      glActiveTexture(GL_TEXTURE0);
-      glBindTexture(GL_TEXTURE_2D, colorBufferID);
-      glTexSubImage2D(GL_TEXTURE_2D, 0,
-                      0, 0,
-                      width, height,
-                      GL_RGBA, GL_UNSIGNED_BYTE, pixels.data());
-      glBindTexture(GL_TEXTURE_2D, 0);
+      renderThread.enqueue( [&] {
+         glActiveTexture(GL_TEXTURE0);
+         glBindTexture(GL_TEXTURE_2D, colorBufferID);
+         glTexSubImage2D(GL_TEXTURE_2D, 0,
+                         0, 0,
+                         width, height,
+                         GL_RGBA, GL_UNSIGNED_BYTE, pixels.data());
+         glBindTexture(GL_TEXTURE_2D, 0);
+      });
+      renderThread.wait_until_nothing_in_flight();
    }
 
    void framebuffer::loadPixels( std::vector<unsigned int> &pixels ) {
       DEBUG_METHOD();
+      renderThread.enqueue( [&] {
       pixels.resize(width*height);
       bind();
       glReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, pixels.data());
+      } );
+      renderThread.wait_until_nothing_in_flight();
    }
 
    void framebuffer::saveFrame(void *surface) {
       DEBUG_METHOD();
-      bind();
-      glReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, surface);
+      renderThread.enqueue( [&] {
+         bind();
+         glReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, surface);
+      } );
+      renderThread.wait_until_nothing_in_flight();
    }
 
    void framebuffer::bind() {
@@ -255,9 +302,16 @@ namespace gl {
 
    mainframe::~mainframe() noexcept {
       DEBUG_METHOD();
-      if ( directVAO ) {
-         glDeleteVertexArrays(1, &directVAO);
-      }
+   }
+
+   void mainframe::release_shader() noexcept {
+      direct = {};
+      renderThread.enqueue( [&] {
+         if ( directVAO ) {
+            glDeleteVertexArrays(1, &directVAO);
+            directVAO = 0;
+         } } );
+      renderThread.wait_until_nothing_in_flight();
    }
 
    mainframe::mainframe(mainframe &&x) noexcept : mainframe()  {
@@ -270,24 +324,30 @@ namespace gl {
       // work
       width = width_;
       height = height_;
-      texture1 = direct.get_uniform("texture1");
-      glGenVertexArrays(1, &directVAO);
+      renderThread.enqueue( [&] {
+         texture1 = direct.get_uniform("texture1");
+         glGenVertexArrays(1, &directVAO);
+      } );
+      renderThread.wait_until_nothing_in_flight();
    }
 
    void mainframe::invert( texture_ptr textureID ) {
-      glBindVertexArray(directVAO);
-      glBindFramebuffer(GL_FRAMEBUFFER, 0);
-      glViewport(0, 0, width, height);
-      glClearColor(0, 0, 0, 1);
-      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-      direct.bind();
-      texture1.set( 15 );
-      glActiveTexture(GL_TEXTURE0+15);
-      glDisable(GL_DEPTH_TEST);
-      glBindTexture(GL_TEXTURE_2D, textureID->get_id());
-      glDrawArrays(GL_TRIANGLES, 0, 6);  // Draw fullscreen quad
-      glEnable(GL_DEPTH_TEST);
-      glBindVertexArray(0);
+      renderThread.enqueue( [directVAO=directVAO, width=width, height=height, textureID,
+                             &direct = direct, &texture1 = texture1 ] {
+         glBindVertexArray(directVAO);
+         glBindFramebuffer(GL_FRAMEBUFFER, 0);
+         glViewport(0, 0, width, height);
+         glClearColor(0, 0, 0, 1);
+         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+         direct.bind();
+         texture1.set( 15 );
+         glActiveTexture(GL_TEXTURE0+15);
+         glDisable(GL_DEPTH_TEST);
+         glBindTexture(GL_TEXTURE_2D, textureID->get_id());
+         glDrawArrays(GL_TRIANGLES, 0, 6);  // Draw fullscreen quad
+         glEnable(GL_DEPTH_TEST);
+         glBindVertexArray(0);
+      } );
    }
 
 } // namespace gl
